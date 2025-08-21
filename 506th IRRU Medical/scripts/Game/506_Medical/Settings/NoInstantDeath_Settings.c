d3a51617720e594026fe804086a5c9e781ea183e
fedoraman737 <monn45888@gmail.com>
2025-08-21 11:55:52 -0500

    Update medical mod to version 2.0.8

--- 506th IRRU Medical/scripts/Game/506_Medical/Settings/NoInstantDeath_Settings.c
+++ 506th IRRU Medical/scripts/Game/506_Medical/Settings/NoInstantDeath_Settings.c
@@ -5,7 +5,7 @@
 class IRRU_NoInstantDeathSettings
 {
 	// Version constant
-	static const string MOD_VERSION = "2.0.7";
+	static const string MOD_VERSION = "2.0.8";
 	
 	// ─── Configurable Settings ─────────────────────────────────────────
 	[Attribute(defvalue: "360", desc: "Time (in seconds) before the unconscious player dies", category: "No Instant Death")]
